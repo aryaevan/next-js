668422859dbc473145bc3b37f688dfef9df3ca94
aryaperdana-guud <Arya.Perdana@guud.company>
2023-12-31 07:55:43 +0700

    login api

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,10 +1,24 @@
+import { sql } from "@vercel/postgres";
+
 export async function POST(request: Request){
-    const res = await request.json();
-    return Response.json(res);
+    // const res = await request.json();
+    // return Response.json(res);
 
-    // const formData = await request.formData();
-    // const username = formData.get('username');
-    // const password = formData.get('password');
+    const formData = await request.formData();
+    const username = formData.get('username')?.toString();
+    const password = formData.get('password')?.toString();
 
-    // return Response.json({username, password});
+    const result = await sql`
+        SELECT password
+        FROM users
+        WHERE username = ${username}`
+
+    const row:any = result.rows;
+    const check = row[0].password;
+
+    if(password && password === check){
+        return Response.json({status: "password match"});
+    }
+
+    return Response.json({password, check, row});
 }
