71518b9161699b35cff7a3e9342c611ef0ce3e95
aryaperdana-guud <Arya.Perdana@guud.company>
2024-01-05 21:16:55 +0700

    simpler product detail screen

---
+++ app/shop/[productid]/description.tsx
@@ -0,0 +1,48 @@
+interface ProductDescriptionProps{
+    product:any,
+};
+export default function ProductDescription({product}:ProductDescriptionProps){
+    return(
+        <>
+          <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+            <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{product.name}</h1>
+          </div>
+
+          
+
+          {/* Details */}
+          <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
+            {/* Description and details */}
+            <div>
+              <h3 className="sr-only">Description</h3>
+
+              <div className="space-y-6">
+                <p className="text-base text-gray-900">{product.description}</p>
+              </div>
+            </div>
+
+            <div className="mt-10">
+              <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
+
+              <div className="mt-4">
+                <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
+                  {product.highlights.map((highlight:any) => (
+                    <li key={highlight} className="text-gray-400">
+                      <span className="text-gray-600">{highlight}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            <div className="mt-10">
+              <h2 className="text-sm font-medium text-gray-900">Details</h2>
+
+              <div className="mt-4 space-y-6">
+                <p className="text-sm text-gray-600">{product.details}</p>
+              </div>
+            </div>
+          </div>
+        </>
+    );
+}
---
+++ app/shop/[productid]/gallery.tsx
@@ -0,0 +1,36 @@
+interface ImageGalleryProps{
+    images:any,
+};
+
+export default function ImageGallery({images} : ImageGalleryProps){
+    return(
+        <div className="bg-blue-400 mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+          <div className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
+            <img
+              src={images[0].src}
+              alt={images[0].alt}
+              className="h-full w-full object-cover object-center"
+            />
+          </div>
+          <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
+            <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
+              <img
+                src={images[1].src}
+                alt={images[1].alt}
+                className="h-full w-full object-cover object-center"
+              />
+            </div>
+            <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
+              <img
+                src={images[2].src}
+                alt={images[2].alt}
+                className="h-full w-full object-cover object-center"
+              />
+            </div>
+          </div>
+          {/* <div className="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
+            
+          </div> */}
+        </div>
+    )
+}
